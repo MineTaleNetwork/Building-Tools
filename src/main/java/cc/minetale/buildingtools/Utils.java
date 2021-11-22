b00a9cc9beab19e8998aed459f21446461890343
Matthew <matik04@gmail.com>
2021-11-22 20:07:41 +0100

    Somewhat temporary, new utilities for serialization to BSON and back

--- src/main/java/cc/minetale/buildingtools/Utils.java
+++ src/main/java/cc/minetale/buildingtools/Utils.java
@@ -2,9 +2,48 @@ package cc.minetale.buildingtools;
 
 import net.minestom.server.command.CommandSender;
 import net.minestom.server.coordinate.Point;
+import net.minestom.server.coordinate.Pos;
 import net.minestom.server.coordinate.Vec;
+import org.bson.Document;
 
 public class Utils {
+
+    //TODO Move these and similar to mLib
+    public static Document vectorToDocument(Vec vec) {
+        return new Document()
+                .append("x", vec.x())
+                .append("y", vec.y())
+                .append("z", vec.z());
+    }
+
+    public static Vec documentToVector(Document document) {
+        return new Vec(
+                document.getDouble("x"),
+                document.getDouble("y"),
+                document.getDouble("z"));
+    }
+
+    public static Document positionToDocument(Pos pos) {
+        return new Document()
+                .append("x", pos.x())
+                .append("y", pos.y())
+                .append("z", pos.z())
+                .append("yaw", pos.yaw())
+                .append("pitch", pos.pitch());
+    }
+
+    public static Pos documentToPosition(Document document) {
+        return new Pos(
+                document.getDouble("x"),
+                document.getDouble("y"),
+                document.getDouble("z"),
+                document.get("yaw", Float.class),
+                document.get("pitch", Float.class));
+    }
+
+    public static Vec toBlockVector(Point point) {
+        return new Vec(point.blockX(), point.blockY(), point.blockZ());
+    }
 
     public static final class Conditions {
 
@@ -27,10 +66,6 @@ public class Utils {
         if(!(player instanceof Builder)) { return null; }
 
         return (Builder) player;
-    }
-
-    public static Vec toBlockVector(Point point) {
-        return new Vec(point.blockX(), point.blockY(), point.blockZ());
     }
 
 }
